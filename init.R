62413619b9a2204733c46587479ac50e46ec137a
valli180 <67402528+valli180@users.noreply.github.com>
2022-03-12 16:41:41 -0800

    Update init.R

--- init.R
+++ init.R
@@ -9,5 +9,5 @@ options(repos=r)
 # ======================================================================
 
 # packages go here
-install.packages(c('dash', 'readr', 'here', 'ggthemes', 'remotes','ggplot2', 'plotly', 'purr'))
+install.packages(c('dash','ggthemes', 'remotes', 'dashHtmlComponents', 'dashCoreComponents', 'ggplot2', 'purr', 'plotly'))
 remotes::install_github('facultyai/dash-bootstrap-components@r-release')
